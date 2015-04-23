b2b6302ffb37913cb3a9bef71fefbe4be11e02ca
thinh <lqthinh92@gmail.com>
2015-04-23 15:45:27 +0700

    delete nham.

---
+++ Source/Quan_Ly_Phong_Mach_Tu/DTO/TTPhongKhamDTO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class TTPhongKhamDTO
+    {
+        private int soLuong;
+        public int SoLuong 
+        {
+            get 
+            {
+                return soLuong;
+            }
+            set
+            {
+                soLuong = value;
+            }
+        }
+
+        private float tienKham;
+        public float TienKham 
+        { 
+            get
+            {
+                return tienKham;
+            } 
+            set
+            {
+                tienKham = value;
+            } 
+        }
+
+        public TTPhongKhamDTO()
+        {
+            soLuong = 30;
+            tienKham = 30000;
+        }
+    }
+}
---
+++ Source/Quan_Ly_Phong_Mach_Tu/QL_PMT_yta/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QL_PMT_yta.usercontrols;
+
+namespace QL_PMT_yta
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Danh_sach_dang_ky_kham main = new Danh_sach_dang_ky_kham();
+            main.Dock = DockStyle.Bottom;
+
+            this.Controls.Add(main);
+        }
+
+        private void btn_thoat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_danhsachdkkham_Click(object sender, EventArgs e)
+        {
+            this.Controls.RemoveAt(1);
+
+            Danh_sach_dang_ky_kham main = new Danh_sach_dang_ky_kham();
+            main.Dock = DockStyle.Bottom;
+
+            this.Controls.Add(main);
+        }
+
+        private void btn_thembenhnhan_Click(object sender, EventArgs e)
+        {
+            this.Controls.RemoveAt(1);
+
+            Them_Benh_Nhan main = new Them_Benh_Nhan();
+            main.Dock = DockStyle.Bottom;
+
+            this.Controls.Add(main);
+        }
+    }
+}
